70676411d4df0ec4276506457a0e65e9041c062a
aberenyi73 <aberenyi@gmail.com>
2017-12-22 17:58:19 -0800

    Ready for submission

--- projects/boston_housing/visuals.py
+++ projects/boston_housing/visuals.py
@@ -125,6 +125,12 @@ def PredictTrials(X, y, fitter, data):
         
         # Result
         print "Trial {}: ${:,.2f}".format(k+1, pred)
+        
 
     # Display price range
-    print "\nRange in prices: ${:,.2f}".format(max(prices) - min(prices))
+    rang = (max(prices) - min(prices))
+    print "\nRange in prices: ${:,.2f}".format(rang)
+    mn = np.array(prices).mean() 
+    print "Range as percent of mean: {:,.0f}%".format((rang / mn)*100)
+    
+    
